2ac5d120a430e751ccecc7c35745f44bcd65ffe9
yyphan <jeromeyao1999@gmail.com>
2021-02-13 15:46:14 +0800

    Open Door when Game Starts

---
+++ Source/UnrealEscapeRoom/OpenDoor.cpp
@@ -0,0 +1,39 @@
+// Yao Yifan 2021
+
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	float MyFloat = 90.f;
+
+	FRotator CurrentRotaion = GetOwner()->GetActorRotation();
+
+	GetOwner()->SetActorRotation(FRotator(CurrentRotaion.Pitch, CurrentRotaion.Yaw + MyFloat, CurrentRotaion.Roll));
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
