9d2d94163b327b28d2f3fd21507fbcd8822b00d2
yyphan <jeromeyao1999@gmail.com>
2021-02-14 12:38:36 +0800

    A Lot of Stuff
    1. Inherited a BP GameMode from original C++ GameMode in order to use our own player pawn
    2. Got player viewpoint (where it is and where it is looking at)
    3. DrawDebugLine (draw a line only for us to see, does not do anything)
    4. LineTraceSingleByObjectType (detect collision with objects of a certain Object Type)

--- Source/UnrealEscapeRoom/Grabber.cpp
+++ Source/UnrealEscapeRoom/Grabber.cpp
@@ -1,7 +1,12 @@
 // Yao Yifan 2021
 
-
+#include "Engine/EngineTypes.h"
+#include "DrawDebugHelpers.h"
+#include "GameFramework/PlayerController.h"
+#include "Engine/World.h"
 #include "Grabber.h"
+
+#define OUT
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -29,6 +34,50 @@ void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompone
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	// Get and log player viewpoint
+	FVector PlayerVPLocation = GetOwner()->GetActorLocation();
+	FRotator PlayerVPRotation = GetOwner()->GetActorRotation();
+
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerVPLocation, 
+		OUT PlayerVPRotation
+	);
+
+	// logging out to test
+	// UE_LOG(LogTemp, Warning, TEXT("VP Location is %s"), *PlayerVPLocation.ToString());
+	// UE_LOG(LogTemp, Warning, TEXT("VP Direction is %s"), *PlayerVPRotation.ToString());
+
+	// Draw a line from player 
+	FVector LineTraceEnd = PlayerVPLocation + PlayerVPRotation.Vector() * Reach;
+
+	DrawDebugLine(
+		GetWorld(), 
+		PlayerVPLocation, 
+		LineTraceEnd, 
+		FColor(0, 255, 0), 
+		false, 
+		0.f,
+		0,
+		5.f
+	);
+
+	FHitResult Hit;
+	// Ray-cast out to a certain distance (Reach)
+	FCollisionQueryParams TraceParams(FName(TEXT("")), false, GetOwner());
+
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		PlayerVPLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParams
+	);
+
+	// See what it hits
+	if(Hit.GetActor()){
+		AActor* HitActor = Hit.GetActor();
+		FString HitName = HitActor->GetName();
+		UE_LOG(LogTemp, Warning, TEXT("The actor being hit is %s"), *HitName);
+	}
 }
 
